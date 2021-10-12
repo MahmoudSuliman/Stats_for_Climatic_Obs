7df32e526295c03be531967457d880f577ceecd5
Mahmoud Suliman <86120671+MahmoudSuliman@users.noreply.github.com>
2021-10-12 19:44:29 +0200

    Update MainScript.py

--- MainScript.py
+++ MainScript.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep  2 15:24:33 2021
+Created on Tue Oct 12 16:29:28 2021
 
-@author: MahmoudSuliman
+@author: Mahmoud Suliman
 """
 
 # =============================================================================
-# Importing libraries
+# importing libraries
 
 import os
 import matplotlib.image as img
@@ -19,27 +19,122 @@ import glob
 from collections import defaultdict
 import PIL
 from PIL import Image, ImageDraw
-Image.MAX_IMAGE_PIXELS = None
 from pathlib import Path
-
-import rasterio
-from rasterio.plot import show
-from rasterio.plot import show_hist
-from rasterio.merge import merge
-from rasterio.mask import mask
-from shapely.geometry import box, Point, mapping
-import geopandas as gpd
-from fiona.crs import from_epsg
-from osgeo import gdal
-import pycrs
-import pyproj
-import json
 from functools import partial
-from shapely.ops import transform
-
-swer='+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
 
 # =============================================================================
-
 # =============================================================================
 
+# defining head directory
+headdir=r'C:\Users\KIDDO\Downloads\SU Study\Traineeship\Urban Heat Island\Data_22T_23P'
+
+# getting a list of all subdirectories within the head directory
+subdir = [f.path for f in os.scandir(headdir) if f.is_dir()]
+
+# getting all climatic csv file directories
+clist=[]
+for i in range(0, len(subdir)):
+    clist1= glob.glob(subdir[i]+'\\*.csv')
+    # print(clist1)
+    clist.append(clist1[0])
+    clist.append(clist1[1])
+  
+# =============================================================================
+# =============================================================================
+# Precipitation data wrangling
+
+# Precipitation Paths
+
+plist=[]
+for i in range(0, len(clist)):
+    if clist[i].find('smhi-opendata_22') == -1:
+        if clist[i].find('T.csv') == -1:
+            if clist[i].find('t.csv') == -1:
+                plist.append(clist[i])
+# praw
+dpraw={}
+for i in range (0,len(plist)):
+    zapath=Path(plist[i])
+    parentpath=zapath.parent.absolute() # gets parent path of directory
+    splitparpath=os.path.split(parentpath) # splits parent path
+    stname = splitparpath[1] # gets station name from split path
+    dpraw[stname]=pd.read_csv(plist[i],  sep=';', names=list('abcdefg'))
+
+# pdata
+dpdata={}
+for i in range (0,len(plist)):
+    zapath=Path(plist[i])
+    parentpath=zapath.parent.absolute() # gets parent path of directory
+    splitparpath=os.path.split(parentpath) # splits parent path
+    stname = splitparpath[1] # gets station name from split path
+    x23=pd.read_csv(plist[i], sep=';', names=list('abcdefg'))
+    startingindex = x23.loc[x23['a']=='Från Datum Tid (UTC)'].index.tolist()
+    dpdata[stname]=pd.read_csv(plist[i], sep=';', header=startingindex)
+
+# tfin
+# study period converted to string array and then to a list
+stperiod=np.arange(1960, 2020).astype(str).tolist()
+
+dpfin={}
+for i in range (0,len(plist)):
+    zapath=Path(plist[i])
+    parentpath=zapath.parent.absolute() # gets parent path of directory
+    splitparpath=os.path.split(parentpath) # splits parent path
+    stname = splitparpath[1] # gets station name from split path
+    x23=pd.read_csv(plist[i], sep=';', names=list('abcdefg'))
+    startingindex = x23.loc[x23['a']=='Från Datum Tid (UTC)'].index.tolist()
+    x232=pd.read_csv(plist[i], sep=';', header=startingindex)
+    dpfin[stname]=x232[x232['Från Datum Tid (UTC)'].str.contains('|'.join(stperiod))] # filtering study years using the regex character '|'
+
+# =============================================================================
+# =============================================================================
+# Temperature data wrangling
+# Temperature Paths
+tlist=[]
+for i in range(0, len(clist)):
+    if clist[i].find('smhi-opendata_23') == -1:
+        if clist[i].find('P.csv') == -1:
+            if clist[i].find('p.csv') == -1:
+                tlist.append(clist[i])
+
+# traw
+dtraw={}
+for i in range (0,len(tlist)):
+    zapath=Path(tlist[i])
+    parentpath=zapath.parent.absolute() # gets parent path of directory
+    splitparpath=os.path.split(parentpath) # splits parent path
+    stname = splitparpath[1] # gets station name from split path
+    dtraw[stname]=pd.read_csv(tlist[i],  sep=';', names=list('abcdefg'))
+
+
+x=pd.read_csv(tlist[0], sep=';', names=list('abcdefg'))
+
+# tdata
+dtdata={}
+for i in range (0,len(tlist)):
+    zapath=Path(tlist[i])
+    parentpath=zapath.parent.absolute() # gets parent path of directory
+    splitparpath=os.path.split(parentpath) # splits parent path
+    stname = splitparpath[1] # gets station name from split path
+    x23=pd.read_csv(tlist[i], sep=';', names=list('abcdefg'))
+    startingindex = x23.loc[x23['a']=='Från Datum Tid (UTC)'].index.tolist()
+    dtdata[stname]=pd.read_csv(tlist[i], sep=';', header=startingindex)
+
+# tfin
+# study period converted to string array and then to a list
+stperiod=np.arange(1960, 2020).astype(str).tolist()
+
+dtfin={}
+for i in range (0,len(tlist)):
+    zapath=Path(tlist[i])
+    parentpath=zapath.parent.absolute() # gets parent path of directory
+    splitparpath=os.path.split(parentpath) # splits parent path
+    stname = splitparpath[1] # gets station name from split path
+    x23=pd.read_csv(tlist[i], sep=';', names=list('abcdefg'))
+    startingindex = x23.loc[x23['a']=='Från Datum Tid (UTC)'].index.tolist()
+    x232=pd.read_csv(tlist[i], sep=';', header=startingindex)
+    dtfin[stname]=x232[x232['Från Datum Tid (UTC)'].str.contains('|'.join(stperiod))] # filtering study years using the regex character '|'
+
+
+
+# =============================================================================
